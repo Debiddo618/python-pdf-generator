d51c7ed4dd6d57757a823d09bba05f18b481d995
Debiddo <dzheng@terpmail.umd.edu>
2023-10-03 02:23:44 -0400

    final code cleaning

--- main.py
+++ main.py
@@ -12,19 +12,27 @@ for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L,", ln=1)
-    pdf.line(10,21, 200, 21)
+    pdf.line(10, 21, 200, 21)
+
+    # making lines
+    for i in range(0, 270, 10):
+        pdf.line(10, 21 + i, 200, 21 + i)
 
     # set the footer
-    pdf.ln(277)
+    pdf.ln(260)
     pdf.set_font(family="Times", style="I", size=8)
-    pdf.set_text_color(180,180,180)
-    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-    for page in range(1,row["Pages"]):
+    for page in range(1, row["Pages"]):
         pdf.add_page()
 
+        # making lines
+        for i in range(0, 270, 10):
+            pdf.line(10, 21 + i, 200, 21 + i)
+
         # set the footer
-        pdf.ln(277)
+        pdf.ln(275)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
